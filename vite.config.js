bf7d8e016317321ca4deb9b221d8a85ffdd713ff
Missclicker <85ping@gmail.com>
2025-04-24 16:47:11 +0300

    define def vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/',
+  server: {
+    port: 5173,
+    open: true,
+    historyApiFallback: true,
+    middleware: (app) => {
+      app.use((req, res, next) => {
+        if (req.url.includes('.')) {
+          next();
+        } else {
+          req.url = '/';
+          next();
+        }
+      });
+    }
+  }
 })
