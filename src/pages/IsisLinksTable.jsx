c364a8565cf51637afc2f48ed04c975006d6fdad
Missclicker <85ping@gmail.com>
2025-04-25 10:17:10 +0300

    add isis table

--- src/pages/IsisLinksTable.jsx
+++ src/pages/IsisLinksTable.jsx
@@ -1,56 +1,73 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, Box } from '@mui/material';
+import { Box, Typography, Paper } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 import axios from 'axios';
+import isisLinksData from '../../isisLinksData.json';
 
 const IsisLinksTable = () => {
   const [links, setLinks] = useState([]);
+  const [pageSize, setPageSize] = useState(25);
 
+  // Commented out API code for future use
+  // useEffect(() => {
+  //   const fetchLinks = async () => {
+  //     try {
+  //       const response = await axios.get('http://localhost:8000/api/isis-links');
+  //       setLinks(response.data);
+  //     } catch (error) {
+  //       console.error('Error fetching ISIS links:', error);
+  //     }
+  //   };
+  //   fetchLinks();
+  // }, []);
+
+  // Use static data for now
   useEffect(() => {
-    // This will be implemented when we have the backend API endpoint
-    const fetchLinks = async () => {
-      try {
-        // const response = await axios.get('http://localhost:8000/api/isis-links');
-        // setLinks(response.data);
-      } catch (error) {
-        console.error('Error fetching ISIS links:', error);
-      }
-    };
-
-    fetchLinks();
+    setLinks(isisLinksData);
   }, []);
 
+  const columns = [
+    { field: 'HOSTNAME', headerName: 'Hostname', width: 150, sortable: true },
+    { field: 'ISIS', headerName: 'ISIS', width: 100, sortable: true },
+    { field: 'IFNAME', headerName: 'Interface', width: 150, sortable: true },
+    { field: 'IF_STATUS', headerName: 'Status', width: 120, sortable: true },
+    { field: 'ADJ_FORM', headerName: 'Adjacency', width: 120, sortable: true },
+    { field: 'BW', headerName: 'Bandwidth', width: 120, sortable: true },
+    { field: 'PEER_COUNT', headerName: 'Peers', width: 100, sortable: true },
+    { field: 'METRIC_L2', headerName: 'L2 Metric', width: 120, sortable: true },
+    { field: 'FW_IP', headerName: 'Forward IP', width: 150, sortable: true },
+    { field: 'GW_IP', headerName: 'Gateway IP', width: 150, sortable: true },
+    { field: 'METRIC_TE', headerName: 'TE Metric', width: 120, sortable: true },
+  ];
+
   return (
-    <Box>
+    <Box sx={{ height: '100%', width: '100%' }}>
       <Typography variant="h4" gutterBottom>
         ISIS Links
       </Typography>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Source Router</TableCell>
-              <TableCell>Target Router</TableCell>
-              <TableCell>Metric</TableCell>
-              <TableCell>Status</TableCell>
-              <TableCell>Last Updated</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {links.map((link) => (
-              <TableRow key={link.id}>
-                <TableCell>{link.id}</TableCell>
-                <TableCell>{link.source_router}</TableCell>
-                <TableCell>{link.target_router}</TableCell>
-                <TableCell>{link.metric}</TableCell>
-                <TableCell>{link.status}</TableCell>
-                <TableCell>{link.last_updated}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Paper sx={{ height: 'calc(100vh - 200px)', width: '100%' }}>
+        <DataGrid
+          rows={links}
+          columns={columns}
+          getRowId={(row) => `${row.HOSTNAME}-${row.IFNAME}`}
+          pageSize={pageSize}
+          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+          rowsPerPageOptions={[10, 25, 50, 100]}
+          pagination
+          disableSelectionOnClick
+          sortingMode="server"
+          sx={{
+            '& .MuiDataGrid-cell': {
+              fontSize: '0.875rem',
+            },
+            '& .MuiDataGrid-columnHeader': {
+              fontSize: '0.875rem',
+              fontWeight: 'bold',
+            },
+          }}
+        />
+      </Paper>
     </Box>
   );
 };
